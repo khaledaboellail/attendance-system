9b28de688160672a208ff8e45f8faa7ae5afce8f
khaledaboellail <khaled.aboellail99@gmail.com>
2026-02-18 10:55:45 +0200

    Update addEmployee.ts

--- pages/api/pages/api/addEmployee.ts
+++ pages/api/pages/api/addEmployee.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { supabaseAdmin } from '../../lib/supabase';
+import { supabaseAdmin } from '../../src/lib/supabase';
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse){
   if(req.method!=='POST') return res.status(405).json({ error:'Method not allowed' });
